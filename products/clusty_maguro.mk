6fccfc205d47f2bf8712075df7b0c746d5925d67
Clust3r <ghys.jeremy@gmail.com>
2012-03-08 22:57:32 +0100

    clusty maguro

--- products/clusty_maguro.mk
+++ products/clusty_maguro.mk
@@ -1,13 +1,24 @@
+# Inherit AOSP device configuration for maguro.
 $(call inherit-product, device/samsung/maguro/full_maguro.mk)
-$(call inherit-product, vendor/clusty/products/common.mk)
-$(call inherit-product, vendor/clusty/products/common_phone.mk)
 
+# Inherit common product files.
+$(call inherit-product, vendor/clusty/configs/common_phone.mk)
+
+# Inherit GSM common stuff
+$(call inherit-product, vendor/clusty/configs/gsm.mk)
+
+# Tuna overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/clusty/overlay/tuna
+PRODUCT_PACKAGE_OVERLAYS += vendor/clusty/overlay/maguro
+
+# Setup device specific product configuration.
 PRODUCT_NAME := Clusty_maguro
 PRODUCT_BRAND := google
 PRODUCT_DEVICE := maguro
 PRODUCT_MODEL := Galaxy Nexus
-PRODUCT_MANUFACTURER := Samsung
+PRODUCT_MANUFACTURER := samsung
 
+PRODUCT_BUILD_PROP_OVERRIDES := BUILD_NUMBER=228551
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=yakju \
     BUILD_ID=IML74K \
@@ -18,15 +29,14 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=android-google \
     ro.com.google.mcc_fallback=262 \
-    ro.setupwizard.mode=OPTIONAL \
     ro.com.android.dataroaming=false \
     drm.service.enabled=true \
     camera.flash_off=0 \
     dalvik.vm.lockprof.threshold=500
 
-PRODUCT_COPY_FILES += \
-    vendor/clusty/prebuilt/toro/bootanimation.zip:system/media/bootanimation.zip
+# Copy maguro specific prebuilt files
+PRODUCT_COPY_FILES +=  \
+    vendor/clusty/prebuilt/tuna/Thinkfree.apk:system/app/Thinkfree.apk \
+	vendor/clusty/prebuilt/toro/bootanimation.zip:system/media/bootanimation.zip
 
-PRODUCT_PACKAGE_OVERLAYS += vendor/clusty/overlay/maguro
-PRODUCT_PACKAGE_OVERLAYS += vendor/clusty/overlay/common
-PRODUCT_PACKAGE_OVERLAYS += vendor/clusty/overlay/gsm
+
